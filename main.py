749f94dd82bee90559c2706be9d3cccc72dcb8bf
Helder-dlt162 <helder.drive162@gmail.com>
2026-05-13 22:13:31 -0500

    Add temporary main test snippet for user repository

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.auth import login
 from db import init
+from db.repositories.users import add_user, get_users, delete_user
+from db.repositories.user_types import add_user_type, get_user_types
 import config
 
 app = FastAPI()
@@ -15,4 +17,11 @@ app.version = app_config["VERSION"]
 def root():
     return {"status": "83348, ok"}
 
-init.init_db()
+# init.init_db()
+data = {"username": "HENeto", "email": "heldere@gmail.com", "password": "12345678", "user_type": 2}
+# data2 = {"name": "Common"}
+
+print(add_user(data))
+# print(get_users(1))
+# print(add_user_type(data2))
+# print(get_user_types())
